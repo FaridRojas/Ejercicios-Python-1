119d5507df0c2b5eedfd0ffe6620a4d20fe4320c
Farid <fcrv35@gmail.com>
2023-05-07 22:31:44 -0500

    Se agrega el proyecto

---
+++ Ejercicio1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+votos = np.random.randint(1, 31, 5000)
+
+votosordenados = np.bincount(votos)
+
+candidatosordenados = np.argsort(votosordenados)[::-1]
+
+for x in candidatosordenados:
+    if x != 0:
+        print("Candidato N°",x," tiene ",votosordenados[x])
+    
---
+++ Ejercico2.py
@@ -0,0 +1,34 @@
+import numpy as np
+import random
+import string
+import names
+
+def nombre():
+    return names.get_full_name()
+
+estudiantes = np.empty((6500, 5), dtype=object)
+
+estudiantes[:, 0] = np.random.randint(2000000, 3000000, size=6500)
+estudiantes[:, 1] = [nombre() for i in range(6500)]
+estudiantes[:, 2] = np.random.uniform(2.8, 5, size=6500)
+estudiantes[:, 3] = np.random.randint(10, 69, size=6500)
+estudiantes[:, 4] = np.random.randint(1980, 2024, size=6500)
+
+codigo_carrera = input("Ingrese el código de la carrera a listar(los codigos de carreras van de 10 a 68): ")
+estudiantes_carrera = estudiantes[estudiantes[:,3] == codigo_carrera]
+estudiantes_promedio = estudiantes[estudiantes[:,2].astype(float) >= 4]
+
+print("Código y nombre de los estudiantes de la carrera ",codigo_carrera," con promedio acumulado igual o mayor a 4:")
+
+for estudiante in estudiantes_promedio:
+    print("Código: ",estudiante[0], ", Nombre: ",estudiante[1])
+    
+print("Total de estudiantes: ",len(estudiantes_promedio))
+
+estudiantes_antes_1990 = estudiantes[estudiantes[:,4].astype(int) < 1990]
+estudiantes_condicionales = estudiantes_antes_1990[estudiantes_antes_1990[:,2].astype(float) < 3.5]
+
+print("Código y nombre de los estudiantes que ingresaron antes de 1990 y están condicionales:")
+
+for estudiante in estudiantes_condicionales:
+    print("Código: ",estudiante[0], ", Nombre: ",estudiante[1])
